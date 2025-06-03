dd2de9ad872a7f2df7f16757ce65fad17ece966a
yunusyerli1 <yunusyerli1@gmail.com>
2025-06-03 17:26:48 +0300

    Add google search console verification code on meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import Navbar from "./_components/navbar";
 export const metadata: Metadata = {
   title: "Yunus Yerli - Portfolio",
   description: "Frontend Developer / Software Engineer Portfolio",
+  other: {
+    'google-site-verification': 'mWpn2ubkbp67CyKmkpAS3dKMyrPX1_7O1sDc5nZRVN4'
+  }
 };
 
 export default function RootLayout({
